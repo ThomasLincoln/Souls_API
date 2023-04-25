28c747cf27d7c6b13fa17f48a9edc01583112a39
Thomas Lincoln <thmas5952@gmail.com>
2023-04-25 20:53:47 -0300

    primeiro rota de teste

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
+const { error } = require('console');
 const express = require('express');
 const mysql = require('mysql2');
 require('dotenv').config();
 const url = require('url');
-
-
 const app = express();
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+
+
+
+
 
 // Configurar rotas
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Estabelecer conexão com o banco de dados
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-console.log('Connected to PlanetScale!');
+app.get('/teste', async (req,res)=>{
+    connection.query('select * from teste; ' ,(error, results) =>{
+        if (error) {
+            console.log(error);
+            connection.end();
+            return;
+          }
+          
+        console.log('Tables:');
+        console.log(results);
+        res.json(results)
+    })
+})
 
-connection.query('SHOW TABLES;', (error, results) => {
-  if (error) {
-    console.log(error);
-    connection.end();
-    return;
-  }
 
-  console.log('Tables:');
-  console.log(results);
-});
 
 // Iniciar servidor
 const port = process.env.PORT || 3000;
