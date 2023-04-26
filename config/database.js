193b08ad0b7bf2cf94421a00a2b8a71403d0a3e6
Thomas Lincoln <thmas5952@gmail.com>
2023-04-26 19:41:05 -0300

    update organização de pastas

--- config/database.js
+++ config/database.js
@@ -1,37 +1,38 @@
-const url = require('url');
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
+import dotenv from 'dotenv';
+import url from 'url';
+import { Sequelize } from 'sequelize';
+dotenv.config();
 
 
+const dbName = process.env.database;
+const dbUser = process.env.username;
+const dbHost = process.env.host;
+const dbPassword = process.env.password;
 
-const dbUrl = process.env.DATABASE_URL;
-const dbConfig = url.parse(dbUrl);
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  dialect: "mysql", //informar o tipo de banco que vamos utilizar
+  host: dbHost, //o host, neste caso estamos com um banco local
+  port: 3306,
+  username: dbUser,
+  password: dbPassword,
+  database: dbName,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
 
-console.log(dbConfig)
-
-module.exports = {
-    dialect: 'mysql',
-    host: process.env.host,
-    port: 3306,
-    username: process.env.username,
-    password: process.env.password,
-    database: process.env.database,
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-      createdAt: 'created_at',
-      updatedAt: 'updated_at',
-    },
-  
-    dialectOptions: {
-      timezone: 'America/Sao_Paulo',
-      ssl: {
-        rejectUnauthorized: true
-      },
-    },
-  
+  dialectOptions: {
     timezone: 'America/Sao_Paulo',
-  };
-  
+    ssl: {
+      rejectUnauthorized: true
+    },
+  },
 
+  timezone: 'America/Sao_Paulo',
+});
+
+
+export default sequelize; //exportar
---
+++ routes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const routes = express.Router();
+
+routes.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
+
+export {routes as default}
